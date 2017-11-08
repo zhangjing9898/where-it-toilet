a3b5a429825c26f23d0b80a2d987cd0596c1a98d
zhangjing9898 <1105249307@qq.com>
2017-11-08 21:04:18 +0800

    finished the map part
    finished the map part

--- server/routes/data.js
+++ server/routes/data.js
@@ -45,7 +45,116 @@ router.post('/write',function (req,res,next) {
       });
     }
     //文件名
-    
-})
+    var type=req.param('type')||'';
+    //关键字段
+    var url=req.param('url')||'';
+    var title=req.param('title')||'';
+    var img=req.param('img')||'';
+    if(!type||!url||!title||!img){
+        return res.send({
+            status:0,
+            info:'提交的字段不全'
+        });
+    }
+    //读取文件
+    var filePath=PATH+type+".json";
+    fs.readFile(filePath,function (err,data) {
+        if(err){
+            return res.send({
+                status:0,
+                info:'读取数据失败'
+            });
+        }
+        var arr=JSON.parse(data.toString());
+        //代表每一条记录
+        var obj={
+            img:img,
+            url:url,
+            title:title,
+            id:guidGenerate(),
+            time:new Date()
+        };
+        arr.splice(0,0,obj);
+        //写入文件
+        var newData=JSON.stringify(arr);
+        fs.writeFile(filePath,newData,function (err) {
+            if(err){
+                return res.send({
+                    status:0,
+                    info:'写入文件失败'
+                });
+            }
+            return res.send({
+                status:1,
+                info:obj
+            });
+        });
+    });
+});
+
+//阅读模块写入接口  后台开发使用
+router.post('/write_config',function (req,res,next) {
+    if(!req.session.user){
+        return res.send({
+            status:0,
+            info:'未鉴权认证'
+        });
+    }
+
+    //可以做防xss攻击
+    //这里先没做
+    var data=req.body.data;
+
+    var obj=JSON.parse(data);
+    var newData=JSON.stringify(obj);
+    //写入
+    fs.writeFile(PATH+'config.json',newData,function (err) {
+        if(err){
+            return res.send({
+                status:0,
+                info:"写入数据失败"
+            });
+        }
+        return res.send({
+            status:1,
+            info:obj
+        });
+    });
+});
+
+//登录接口
+router.post("/login",function (req,res,next) {
+    //用户名 密码 验证码
+    var username=req.body.username;
+    var password=req.body.password;
+
+    //todo:对用户名 密码进行校验
+    //xss 处理 判空
+
+    //密码加密 使用md5(md5(password+'随机字符串'))
+    //加密后的密码 可以写入JSON文件
+
+    if(username==='admin'&&password==='123456'){
+        req.session.user={
+            username:username
+        };
+        return res.send({
+            status:1
+        });
+    }
+    return res.send({
+        status:0,
+        info:'登录失败'
+    });
+});
+
+//guid
+function guidGenerate() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    }).toUpperCase();
+}
 
 module.exports = router;
